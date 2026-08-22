68faa785c59ae0deb6becdf0bff6418a8e8cc6b6
ccoldpaper001 <ccoldpaper001@users.noreply.github.com>
2026-08-22 15:38:48 +0800

    left-align checkbox/option labels

--- append-css.ps1
+++ append-css.ps1
@@ -2,10 +2,9 @@ $enc = [System.Text.Encoding]::UTF8
 $p = 'C:/Users/cold paper/.zcode/workspace/default/frequency-channel/toolbox/scoped.css'
 $add = @"
 
-/* 输入框与内容卡片宽度自适应容器 */
-#toolbox-root input:not([type='checkbox']):not([type='radio']):not([type='button']):not([type='submit']):not([type='file']),
-#toolbox-root textarea,
-#toolbox-root select{width:100%;max-width:100%;box-sizing:border-box}
+/* 复选框/选项文字统一左对齐 */
+#toolbox-root label{text-align:left;justify-content:flex-start}
+#toolbox-root input[type='checkbox'],#toolbox-root input[type='radio']{width:auto;flex-shrink:0}
 "@
 [System.IO.File]::AppendAllText($p, $add, $enc)
 Write-Output 'done'
